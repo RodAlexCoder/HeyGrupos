0999d6b34476ec2767212b8429cd0f1b9830c35b
RodAlexCoder <nasc.aleex@gmail.com>
2022-03-03 14:57:45 -0300

    Criando a tela de SignIn e ChatRoom

--- src/pages/ChatRoom/index.js
+++ src/pages/ChatRoom/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Button} from 'react-native'
+import auth from '@react-native-firebase/auth'
+import {useNavigation} from '@react-navigation/native'
 
 export default function ChatRoom() {
+
+  const navigation = useNavigation()
+
   return (
-    <View>
+    <View style={styles.container}>
           <Text>Tela ChatRoom!</Text>
+          <Button title='Login' onPress={() => navigation.navigate("SignIn")}/>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
 })
 
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,15 +1,91 @@
-import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, TextInput , SafeAreaView, Platform} from 'react-native'
 
 export default function SignIn() {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
   return (
-    <View>
-          <Text>Tela Login!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.logo}>Hey Grupos!</Text>
+
+      <Text style={{ marginBottom: 20 }}>ajude, colabore, faça networking!</Text>
+
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={(text) => setName(text)}
+        placeholder="Qual o Seu Nome"
+        placeholderTextColor='#9999B'
+      />
+
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        placeholder="Qual o Seu Email"
+        placeholderTextColor='#9999B'
+      />
+
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        placeholder="Sua Senha"
+        placeholderTextColor='#9999B'
+      />
+
+      <TouchableOpacity style={styles.buttonLogin}>
+            <Text style={styles.buttonText}>Acessar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+          <Text>Criar uma nova conta</Text>
+      </TouchableOpacity>
+
+    </SafeAreaView>
   )
 }
 
+
 const styles = StyleSheet.create({
-    
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#FFF'
+  },
+
+  logo: {
+    marginTop: Platform.OS === 'android' ? 55 : 80,
+    fontSize: 28, 
+    fontWeight: 'bold'
+  },
+
+  input: {
+    color: '#121212',
+    backgroundColor: '#EBEBEB',
+    width: '90%',
+    borderRadius: 6,
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    height: 50
+  },
+  buttonLogin: {
+    width: '90%',
+    backgroundColor: '#121212',
+    height: 50,
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    marginBottom: 10,
+    borderRadius: 6
+  },
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold', 
+    fontSize: 19
+  }
 })
 
