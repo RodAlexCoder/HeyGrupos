51be1cb0d121bf57abc896c4575f168eda2d5d94
RodAlexCoder <nasc.aleex@gmail.com>
2022-03-04 13:00:59 -0300

    Criando mensagens sincronizadas

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,14 +1,17 @@
 import React, {useEffect, useState} from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, FlatList, SafeAreaView, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity} from 'react-native'
 import auth from '@react-native-firebase/auth'
 import firestore from '@react-native-firebase/firestore'
+import ChatMessage from '../../components/ChatMessage'
+import Feather from 'react-native-vector-icons/Feather'
 
 export default function Messages({route}) {
 
   const {thread} = route.params
   const [messages, setMessages] = useState([])
+  const [input , setInput] = useState('')
 
-  const user = auth().currentUser.toJSON
+  const user = auth().currentUser.toJSON()
 
   useEffect(()=> {
       const unsubscribeListener = firestore().collection('MESSAGE_THREAD')
@@ -42,14 +45,112 @@ export default function Messages({route}) {
 
   }, [])
 
+  async function handleSend (){
+    if(input === '') return
+
+    await firestore()
+    .collection('MESSAGE_THREAD')
+    .doc(thread._id)
+    .collection('MESSAGES')
+    .add({
+      text: input,
+      createdAt: firestore.FieldValue.serverTimestamp(),
+      user: {
+        _id: user.uid,
+        displayName: user.displayName
+      }
+    })
+
+    await firestore()
+    .collection('MESSAGE_THREAD')
+    .doc(thread._id)
+    .set(
+      {
+        lastMessage: {
+          text: input,
+          createdAt: firestore.FieldValue.serverTimestamp()
+        }
+      }, {merge: true}
+    )
+
+    setInput('')
+  }
+
   return (
-    <View>
-          <Text>Tela Messages!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+          <FlatList 
+          style={{width: '100%'}}
+          data={messages}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => <ChatMessage data={item} />}
+          inverted={true}
+          />
+
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{width: '100%'}}
+      keyboardVerticalOffset={100}
+    >
+      <View style={styles.containerInput}>
+
+        <View style={styles.mainContainerInput}>
+            <TextInput 
+              placeholder='Sua Mensagem...'
+              value={input}
+              onChangeText={(text) => setInput(text)}
+              style={styles.textInput}
+              multiline={true}
+              autoCorrect={false}
+          />
+        </View>
+        <TouchableOpacity onPress={handleSend}>
+            <View style={styles.buttonContainer}>
+                  <Feather name='send' size={22} color='#FFF'/>
+            </View>
+        </TouchableOpacity>
+      </View>
+
+
+    </KeyboardAvoidingView>
+
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-    
+    container:{
+      flex: 1, 
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+
+    containerInput: {
+      flexDirection: 'row',
+      margin: 10,
+      alignItems: 'center',
+    },
+    mainContainerInput: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: '#FFF',
+      flex: 1,
+      borderRadius: 25,
+      marginRight: 10
+    },
+
+    textInput: {
+      flex: 1,
+      marginHorizontal: 10,
+      maxHeight: 130,
+      minHeight: 48
+    },
+    buttonContainer: {
+      backgroundColor: '#51c880',
+      height: 48,
+      width: 48,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 25
+    }
 })
 
